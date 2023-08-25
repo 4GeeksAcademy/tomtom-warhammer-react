f622237c612b836af296e3cae0826a0382881e03
tomtom12065 <100985000+tomtom12065@users.noreply.github.com>
2023-08-25 08:31:42 +0000

    working on the clock

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-
+// import './index.css';
 //include images into your bundle
 
 import CardDeckBuilder from "./cardgenerator";
-import ChessClock from "./ChessClock";
+// import ChessClock from "./ChessClock";
 //create your first component
 const Home = () => {
 	return <div className="container">
 				
-				<ChessClock></ChessClock>
+				{/* <ChessClock></ChessClock> */}
 				<div className="top box">
 					<h1>defender</h1>
 					<CardDeckBuilder></CardDeckBuilder>
